1e6b0f04020708dc8d1d37b9f55597abb5adafe3
Dorian Masquelier <dorian.masquelier@gmail.com>
2022-04-01 19:17:57 +0200

    clean code

--- src/App.js
+++ src/App.js
@@ -13,47 +13,77 @@ function App() {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
-  const [counter, setCounter] = useState(0);
-
-  const [panier, setPanier] = useState([]);
-
-  const minusSign = () => {
-    const newCounter = [...counter];
-    newCounter.push(0);
-    setCounter(newCounter - 1);
-  };
-
-  const addSign = () => {
-    const newCounter = [...counter];
-    newCounter.push(0);
-    setCounter(newCounter + 1);
-  };
+  const [cart, setCart] = useState([]);
 
   console.log("Render !");
 
-  const fetchData = async () => {
+  useEffect(() => {
     try {
-      const response = await axios.get(
-        "https://deliveroo-backend-project-v1.herokuapp.com/"
-      );
-      console.log(response.data);
-      setData(response.data);
-      setIsLoading(false);
+      const fetchData = async () => {
+        const response = await axios.get(
+          // "https://deliveroo-backend-project-v1.herokuapp.com/"
+          "http://localhost:4000"
+        );
+        console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      };
+      fetchData();
     } catch (error) {
       console.log(error.response);
     }
+  }, []);
+
+  let subTotal = 0;
+
+  // à chaque modification d'un état, ce morceau de code est relancé
+  cart.forEach((cartItem) => {
+    subTotal = subTotal + Number(cartItem.price) * cartItem.quantity;
+  });
+
+  let total = subTotal + 2.5;
+
+  const addToCart = (meal) => {
+    // push le titre du menu dans cart
+    const newCart = [...cart];
+    // Est-ce que meal est déjà présent dans cart ?
+
+    const exist = newCart.find((elem) => elem.id === meal.id);
+    // console.log("L'élément trouvé ====> ", exist);
+
+    if (exist) {
+      // incrémenter exist.quantity
+
+      exist.quantity++;
+    } else {
+      // ajouter une clé quantity dans meal
+
+      meal.quantity = 1;
+      newCart.push(meal);
+    }
+    setCart(newCart);
   };
 
-  useEffect(() => {
-    console.log("useEffect est déclenchée...");
+  const substractFromCart = (meal) => {
+    const newCart = [...cart];
+    // On cherche dans newCart l'objet pour lequel on veut décrémenter la quantité
 
-    fetchData();
-  }, []);
+    const exist = newCart.find((elem) => elem.id === meal.id);
+
+    if (exist.quantity === 1) {
+      // supprimer l'élément du tableau
+      // trouver l'index de l'élément à supprimer
+      const index = newCart.indexOf(exist);
+      newCart.splice(index, 1);
+    } else {
+      exist.quantity--;
+    }
+    setCart(newCart);
+  };
 
   return isLoading ? (
     <span>En cours de chargement...</span>
   ) : (
-    // Code de la page
     <div className="body">
       <div className="header">
         <div className="border-logo">
@@ -68,55 +98,49 @@ function App() {
 
       <div className="container">
         <main>
+          {/* Liste de catégories */}
           <div className="restaurantOffers-section">
             <div div className="restaurantOffers">
-              {data.categories.map((mealType, index) => {
+              {data.categories.slice(0, 6).map((category, index) => {
                 return (
                   <div className="mealPropositions">
-                    <h2 key={index}>{mealType.name}</h2>
+                    <h2 key={index}>{category.name}</h2>
                     <div className="meal-bloc">
-                      {mealType.meals.map((mealOffers, index) => {
+                      {category.meals.map((meal, index) => {
                         return (
                           <div
                             className="meal-description-illustration"
+                            key={meal.id}
                             onClick={() => {
-                              console.log(
-                                "J'ai cliqué sur le produit =======>",
-                                mealOffers
-                              );
-
-                              // Mise à jour du panier
-                              const newPanier = [...panier];
-                              newPanier.push(mealOffers);
-                              setPanier(newPanier);
+                              addToCart(meal);
                             }}
                           >
                             <div className="mealOffer">
                               <div className="bloc-description">
-                                <h3>{mealOffers.title}</h3>
+                                <h3>{meal.title}</h3>
                                 <div className="mealOffer-description">
-                                  {mealOffers.description ? (
-                                    <span>{mealOffers.description}</span>
+                                  {meal.description ? (
+                                    <span>
+                                      {meal.description.slice(0, 50)}...
+                                    </span>
                                   ) : (
                                     <span></span>
                                   )}
                                 </div>
                                 <div className="price-favIcon">
-                                  <p>{mealOffers.price} €</p>
-                                  {mealOffers.popular === true ? (
+                                  <p>{meal.price} €</p>
+                                  {meal.popular && (
                                     <div className="fav-icon">
                                       <i class="fas fa-star"></i>{" "}
                                       <span>Populaire</span>
                                     </div>
-                                  ) : (
-                                    <span></span>
                                   )}
                                 </div>
                               </div>
                             </div>
-                            {mealOffers.picture && (
+                            {meal.picture && (
                               <img
-                                src={mealOffers.picture}
+                                src={meal.picture}
                                 className="meal-illustration"
                                 alt="meal-illustration"
                               />
@@ -130,7 +154,7 @@ function App() {
               })}
             </div>
             <div className="panier">
-              {panier.length < 1 ? (
+              {cart.length < 1 ? (
                 <>
                   <div className="div-empty-product-button">
                     <button id="empty-product-button">
@@ -142,50 +166,53 @@ function App() {
                   </div>
                 </>
               ) : (
-                <div className="div-product-button">
-                  <button id="validate-product-button">
-                    Valider mon panier
-                  </button>
-                  <div className="panier-plein">
-                    {data.categories.map((mealType, index) => {
-                      return (
-                        <>
-                          {mealType.meals.map((meal, index) => {
-                            return (
-                              <div className="panier-product-selection">
-                                <div className="buttons-product-selection">
-                                  <div className="button-circle">
-                                    <button
-                                      className="minus-button"
-                                      // onClick={}
-                                    >
-                                      -
-                                    </button>
-                                  </div>
-                                  <button class="counter">{counter}</button>
-                                  <div className="button-circle">
-                                    <button
-                                      className="plus-button"
-                                      // onClick={}
-                                    >
-                                      +
-                                    </button>
-                                  </div>
-                                  <p>{meal.title}</p>
-                                </div>
-                                <div>{meal.price} €</div>
-                              </div>
-                            );
-                          })}
-                          ;
-                        </>
-                      );
-                    })}
-                    ;
-                    <div className="sous-total-bloc-panier">
-                      Sous-total & Frais de livraison
+                <div className="panier-plein">
+                  {cart.map((elem, index) => {
+                    return (
+                      <div className="panier-product-selection" key={elem.id}>
+                        <div className="buttons-product-selection">
+                          <div className="button-circle">
+                            <button
+                              className="minus-button"
+                              onClick={() => substractFromCart(elem)}
+                            >
+                              -
+                            </button>
+                          </div>
+                          <button class="counter">{elem.quantity}</button>
+                          <div className="button-circle">
+                            <button
+                              className="plus-button"
+                              onClick={() => addToCart(elem)}
+                            >
+                              +
+                            </button>
+                          </div>
+                          <p>{elem.title}</p>
+                        </div>
+                        <div className="product-price">
+                          {(Number(elem.price) * elem.quantity).toFixed(2)} €
+                        </div>
+                      </div>
+                    );
+                  })}
+                  <div className="total-price-bloc-panier">
+                    <div className="sous-total">
+                      <span>Sous-total :</span>{" "}
+                      <span>{subTotal.toFixed(2)} €</span>
                     </div>
-                    <div className="total-bloc-panier">Total</div>
+                    <div className="frais-de-livraison">
+                      <span>Frais de livraison :</span> <span>2.50 €</span>
+                    </div>
+                    <div className="total-panier">
+                      <span>Total : </span>
+                      <span>{total.toFixed(2)} €</span>
+                    </div>
+                    <div className="button-validate-container">
+                      <button id="validate-product-button">
+                        Valider mon panier
+                      </button>
+                    </div>
                   </div>
                 </div>
               )}
--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -5,13 +5,11 @@ const Restaurant = (props) => {
         <h1 className="restaurant-title">{props.name}</h1>
         <p className="restaurant-description">{props.description}</p>
       </div>
-      {/* <div className="test"> */}
       <img
         className="restaurant-illustration"
         src={props.picture}
         alt="restaurant-description"
       />
-      {/* </div> */}
     </div>
   );
 };
